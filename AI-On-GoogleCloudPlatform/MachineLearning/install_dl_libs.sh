9b0aced498f80457aae5aa2d5eb764386cad00ba
John <John>
2019-11-04 23:02:27 +0000

    AI on GCP with Webscraping and UI

---
+++ AI-On-GoogleCloudPlatform/MachineLearning/install_dl_libs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt-get update
+apt-get install -y graphviz
+apt-get install libhdf5-dev -y
+pip install pydot
+pip install graphviz
+pip install pydotplus
+pip install opencv-contrib-python
+pip install orderedset
+pip install xmltodict
+pip install xlrd
+pip install keras
